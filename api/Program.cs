29d90f6779b6a7241bdfe6c0b27389ba756d8a5c
vietanh <vietanhstar1@gmail.com>
2024-05-09 08:19:40 +0700

    [WebAPI] Version 2 - Add Controllers/StockController.cs  :  receive request from Swaggen & take data from ApplicationDBContext.cs then response - Add Data/ApplicationDBContext.cs    :  import Entity Framework, creat class inheritance DbContext to manage CRUD Database mapping Models - Add class above into builder.Servies.AddDbContext<>() & add ConntextionString into appsettings.json - dotnet ef migration add init   ->  generate Migrations folder - dotnet ef database update      ->  update config to database structure from source code - dotnet watch run               ->  run program  (Swagger will appear to show API)

--- api/Program.cs
+++ api/Program.cs
@@ -1,9 +1,20 @@
+using api.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// VIET ANH
+builder.Services.AddDbContext<ApplicationDBContext>(options => {
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+// VIET ANH
 
 var app = builder.Build();
 
@@ -36,9 +47,11 @@ app.UseHttpsRedirection();
 // .WithName("GetWeatherForecast")
 // .WithOpenApi();
 
+// if do not include => Swagger do not work to Controller
+app.MapControllers();
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+// record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+// {
+//     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+// }
--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5483e1d66d590aa3a877c892403d1f45c34a1e6e")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
